e88acd22c8dae474ef180b567629e0ca5e4b1c90
Sivan Mehta <skmehta@andrew.cmu.edu>
2015-05-13 22:43:44 -0400

    added walking and testing apparatus for walking, as well as basic distance metric

--- roaming.py
+++ roaming.py
@@ -8,17 +8,60 @@ import random
 # Assumptions made:
 # there are two people looking for eachother
 # both of them, if they are moving, are moving in a random direction each step
-# each has a vision of 1/10th of the overall size of the "park"
+# each has a vision of 1/10th of the width of the "park"
 # the park is a square with no obstacles
 
+def walk(person, parkSize):
+    person[0] = (abs(person[0] + random.randint(-1,1))) % (parkSize + 1)
+    person[1] = (abs(person[1] + random.randint(-1,1))) % (parkSize + 1)
+
+# this is a really lazy test, as it tests via exhaustion
+def testWalk():
+
+    # test small park
+    person = [5, 5]
+    parkSize = 10
+
+    for x in xrange(10000):
+        walk(person, parkSize)
+        assert person[0] >= 0 and person[0] <= parkSize
+        assert person[1] >= 0 and person[1] <= parkSize
+
+    # test large park
+    person = [25, 25]
+    parkSize = 50
+
+    for x in xrange(10000):
+        walk(person, parkSize)
+        assert person[0] >= 0 and person[0] <= parkSize
+        assert person[1] >= 0 and person[1] <= parkSize
+
+    # start in corner
+    person = [0, 0]
+    parkSize = 50
+
+    for x in xrange(10000):
+        walk(person, parkSize)
+        assert person[0] >= 0 and person[0] <= parkSize
+        assert person[1] >= 0 and person[1] <= parkSize
+
+def withinSight(person1, person2):
+    return ((person1[0] - person2[0])**2 + (person2[1] - person1[1])**2) < 10
+
 def walkingTrial(parkSize = 10):
-    number = 0
+    steps = 0
 
     # let's assume they start in the same place
     husband = [random.randint(1, parkSize), random.randint(1, parkSize)]
     wife = [random.randint(1, parkSize), random.randint(1, parkSize)]
 
-    return number
+    while(not withinSight(husband, wife)):
+        walk(husband, parkSize)
+        walk(wife, parkSize)
+
+        steps += 1
+
+    return steps
 
 def runTrials(trialCount = 1000):
     outcomes = [0,0]
@@ -31,5 +74,7 @@ def runTrials(trialCount = 1000):
 
     print "They found each other in %d/%d trials" % (outcomes[0], outcomes[1])
 
+# testWalk()
 
 runTrials()
+
